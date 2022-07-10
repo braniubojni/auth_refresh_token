1d43d32097751f51ac0bdd709ac6b6b8f9ee029a
tagavor <erik95harutyunyan@gmail.com>
2022-07-11 00:01:55 +0400

    eh/feat: routes created but not implemented

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const router = require('./routers/index.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser);
 app.use(cors());
+app.use('/api', router);
 
 const start = async () => {
   try {
