f157bd253ce12428605213cda1c5d5e33699c23e
tagavor <erik95harutyunyan@gmail.com>
2022-07-12 18:43:59 +0400

    eh/fix: issue with cookie parser call, have another issue on registration

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const PORT = process.env.PORT || 6000;
-  app.use(cookieParser);
+  app.use(cookieParser()); // Need 2 call parser
   app.setGlobalPrefix('/api');
 
   await app.listen(PORT, () => console.log(`Server at ${PORT}`));
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,29 +1,24 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { CreateUserDto } from './dto/create-user.dto';
-import { IRegReturn } from './user.interface';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {
-    console.log('Worked')
-  }
+  constructor(private readonly userService: UserService) {}
 
   @Post('registration')
   async registration(
-    // @Req() req: Request,
-    // @Res({ passthrough: true }) res: Response,
-    @Body() body: any,
-  ): Promise<any> {
-    // const userDto: CreateUserDto = req.body;
-    // const userData = await this.userService.registration(userDto);
-    // res.cookie('refreshToken', userData.refreshToken, {
-    //   maxAge: 2592000000, // 30 day
-    //   httpOnly: true,
-    // });
-    console.log(body, 'worked');
-    return 'OK';
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<Response> {
+    const userDto: CreateUserDto = req.body;
+    const userData = await this.userService.registration(userDto);
+    res.cookie('refreshToken', userData.refreshToken, {
+      maxAge: 2592000000, // 30 day
+      httpOnly: true,
+    });
+    return res.json(userData);
   }
 
   @Post('login')
